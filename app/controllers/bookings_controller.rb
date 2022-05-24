a94b8fae576bb98ce81178820e070f8948ae3efb
olwyn hogan <hoganol@tcd.ie>
2022-05-24 15:46:04 +0200

    new booking form done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,26 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    profile = ChefProfile.find(params[:chef_profile_id])
+    @booking.user = current_user
+    @booking.chef_profile = profile
+    if @booking.save!
+      redirect_to bookings_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :duration, :status)
+  end
 end
