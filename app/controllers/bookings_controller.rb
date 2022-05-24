f627c271824392ee021cb704664f778753e573ab
Victor Benito <victorbenito8@gmail.com>
2022-05-24 16:05:01 +0200

    m

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
+    raise
   end
 end
--- app/controllers/chef_profiles_controller.rb
+++ app/controllers/chef_profiles_controller.rb
@@ -1,5 +1,5 @@
 class ChefProfilesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_profile, only: [:show]
 
   def index
