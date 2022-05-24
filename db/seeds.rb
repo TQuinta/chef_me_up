c8485e2853c598f48ec004764855a2e3fc77d37d
Tomas Quintanilha <tquinta@alumni.ie.edu>
2022-05-24 11:17:00 +0200

    basic seeds complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'date'
+
+Booking.destroy_all
+ChefProfile.destroy_all
+User.destroy_all
+
+puts "creating users"
+20.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: '123456',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_name
+  )
+  user.save!
+  puts "Created #{user.first_name}"
+end
+
+puts "users created"
+
+puts "creating chef profiles"
+
+5.times do
+  user = User.all.sample
+  chef = ChefProfile.new(
+    title: user.first_name,
+    description: Faker::Food.description,
+    category: Faker::Food.ethnic_category,
+    user: user
+  )
+  chef.save!
+  puts "Created #{chef.title}"
+end
+
+puts "chef profiles created"
+
+puts "creating bookings"
+
+5.times do
+  user = User.all.sample
+  chef = ChefProfile.all.sample
+  booking = Booking.new(
+    date: DateTime.now,
+    duration: rand(1...20),
+    status: %w[pending rejected confirmed].sample,
+    user: user,
+    chef_profile: chef
+  )
+  booking.save!
+  puts "Created booking for #{booking.user.first_name}"
+end
+
+puts "bookings created"
