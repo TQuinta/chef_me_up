0dff66d65eb586953957ec45886682a7cc5a47b1
Tomas Quintanilha <tquinta@alumni.ie.edu>
2022-05-25 11:33:46 +0200

    added url columns for picture for the users and chef profiles tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_131649) do
+ActiveRecord::Schema.define(version: 2022_05_25_092918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_131649) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "meal_picture_url"
     t.index ["user_id"], name: "index_chef_profiles_on_user_id"
   end
 
@@ -48,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_131649) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
